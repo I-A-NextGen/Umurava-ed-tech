344c00534d9779b45eb266a998d5f257edec60a9
DUSHIME1212 <mdonavan33@gmail.com>
2025-02-03 12:16:15 +0200

    delete store.js i

--- src/Redux/features/authSlice.ts
+++ src/Redux/features/authSlice.ts
@@ -32,7 +32,7 @@ export const authSlice = createSlice({
 
 export const { setUser, logout } = authSlice.actions;
 
-// Selectors
+
 export const selectAuth = (state: RootState) => state.auth;
 export const selectUser = (state: RootState) => state.auth.user;
 export const selectIsAuthenticated = (state: RootState) => state.auth.isAuthenticated;
