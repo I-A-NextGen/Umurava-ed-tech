70ed60d098bd6faaed7f392ddb08652db7f5925f
DUSHIME1212 <mdonavan33@gmail.com>
2025-02-08 19:23:08 +0200

    fix: correct typo in NotFound component message

--- src/app/_components/NotFound.tsx
+++ src/app/_components/NotFound.tsx
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div className='h-screen flex flex-col items-center justify-center'>
         <h1 className='text-4xl'>404 Not Found</h1>
-        <p>The page you&apos;re looking for doesn&apos;t exist!</p>
+        <p>The page you&apos;re looking for doesn &apos;t exist!</p>
     </div>
   )
 }
