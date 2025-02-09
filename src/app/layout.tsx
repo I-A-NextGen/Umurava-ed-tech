b05a4c91dc230a581ad78050e893a4e1df9c4e56
DUSHIME1212 <mdonavan33@gmail.com>
2025-02-09 17:50:33 +0200

    fix(layout): remove unused font import and simplify body class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { type Metadata } from "next";
 import { Inter } from "next/font/google";
 import TopLoader from "@/components/TopLoader";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Ed-Tech",
@@ -18,7 +18,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body >
         <ReduxProvider>
           <TopLoader />
           <ToastContainer />
